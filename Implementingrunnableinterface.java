6da88d1e6165fc312ecd82a6a28da675819223a6
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-15 19:24:06 +0530

    Add files via upload

---
+++ Implementingrunnableinterface.java
@@ -0,0 +1,14 @@
+class a implements Runnable
+{
+    public void run(){
+        System.out.println("hello i am thread");
+    }
+}
+public class Implementingrunnableinterface{
+    public static void main(String[] args) {
+        a r=new a();
+        Thread s=new Thread(r);
+        s.start();
+        s.run();
+    }
+}
